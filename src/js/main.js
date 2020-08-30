e342684948b1f5e7f69004f9f3880476e683ce74
ZenVega <50147356+ZenVega@users.noreply.github.com>
2020-08-30 19:32:58 +0200

    removed objects for mobile version

--- src/js/main.js
+++ src/js/main.js
@@ -14,7 +14,23 @@ window.onscroll = function() {
 
 // REDUCE TEXTS IN MOBILE
 
-const booksText = document.querySelector('#books-description');
-const projectsText = document.querySelector('#projects-description');
-const educationText = document.querySelector('#education-description');
-const booksText = document.querySelector('#me-text-2');
+const booksText = document.querySelector('#books-description-2');
+const projectsText = document.querySelector('#projects-description-2');
+const meText = document.querySelector('#me-description-2');
+
+const booksBtn = document.querySelector('#books-more-btn')
+const meBtn = document.querySelector('#me-more-btn')
+const projectsBtn = document.querySelector('#projects-more-btn')
+
+booksBtn.addEventListener('click', () => {
+  booksBtn.style.display= "none";
+  booksText.style.display="block"
+})
+projectsBtn.addEventListener('click', () => {
+  projectsBtn.style.display= "none";
+  projectsText.style.display="block"
+})
+meBtn.addEventListener('click', () => {
+  meBtn.style.display= "none";
+  meText.style.display="block"
+})
